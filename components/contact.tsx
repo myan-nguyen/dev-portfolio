8628aaa24bf8a2eb3eb6768a21af9965c7d9af1d
myan-nguyen <myan_nguyen@brown.edu>
2025-01-25 15:36:31 -0500

    Updated resume.

--- components/contact.tsx
+++ components/contact.tsx
@@ -64,7 +64,7 @@ export default function Contact() {
                 </div>
                 <div className='w-1/2 pl-20'>
                     <iframe
-                        src="https://drive.google.com/file/d/1wqPefsdavLsEF8kgatPL4PNIXxRia6JK/preview?usp=sharing"
+                        src="https://drive.google.com/file/d/1LWUW-Oq9L5q0uiov8DqFgwLbHTVDZFCR/preview?usp=sharing"
                         width="561px"
                         height="726px"
                         loading="lazy">
